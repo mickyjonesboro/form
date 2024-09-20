1a89d23d2a7fa27e973aa64d90b5dd8cfc876ebe
mickyjonesboro <mickyjonesboro@gmail.com>
2024-09-20 22:47:26 +0100

    Third Commit

--- frontend/components/nav.jsx
+++ frontend/components/nav.jsx
@@ -5,7 +5,7 @@ import Logo from "/public/ssalogo.png";
 export default function Navbar() {
   return (
     <nav className=" px-6 lg:px-20 pt-10">
-      <Image src={Logo} />
+      <Image src={Logo} alt="SSA logo"/>
       <Link href="/" className="py-6">Sign In</Link>
       <p>
         Accounts created <b>before</b> September 18, 2021 should enter a
